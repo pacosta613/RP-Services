964ddd98251fc8cd4b3daff940a5a3e1e2483710
pacosta613 <pedro.acosta613@gmail.com>
2016-12-16 08:51:07 -0500

    testing google omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,26 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
   after_create :send_admin_mail
-  
+  has_and_belongs_to_many :oauth_credentials
+
   def send_admin_mail
     UserMailer.send_welcome_email(self).deliver_later
   end
+
+  def self.from_omniauth(access_token)
+    raise "Hello".inspect
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
+  end
+
 
 end
