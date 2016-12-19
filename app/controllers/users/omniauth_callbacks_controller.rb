2f1005b23d1fd3d453a6ce1b774e3902158b5703
pacosta613 <pedro.acosta613@gmail.com>
2016-12-18 19:34:16 -0500

    Omniauth google is working. Now user can sign in with their google account.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+    # if auto_hash = request.env["omniauth.auth"]
+    #   user = find_or_create_by_omniauth(auto_hash)
+    #   session[:user_id] = user.id
 
-      if @user.persisted?
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in_and_redirect @user, :event => :authentication
-      else
-        session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+    #   redirect_to root_path
+    # else
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,25 +6,24 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
   after_create :send_admin_mail
-  has_and_belongs_to_many :oauth_credentials
 
   def send_admin_mail
     UserMailer.send_welcome_email(self).deliver_later
   end
 
   def self.from_omniauth(access_token)
-    raise "Hello".inspect
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
-
-    # Uncomment the section below if you want users to be created if they don't exist
+    # data = access_token.info
+    # user = User.where(:email => data["email"]).first
     # unless user
     #     user = User.create(name: data["name"],
     #        email: data["email"],
     #        password: Devise.friendly_token[0,20]
     #     )
     # end
-    user
+    # user
+    self.where(:email => access_token["info"]["email"]).first_or_create do |user|
+      user.password = SecureRandom.hex
+    end
   end
 
 
