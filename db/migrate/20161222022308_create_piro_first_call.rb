265c5d331f6dfd63f11dff97275c04f27dcf2f95
pacosta613 <pedro.acosta613@gmail.com>
2016-12-21 21:29:54 -0500

    Created the ActiveRecord DB for piro first call form.

---
+++ db/migrate/20161222022308_create_piro_first_call.rb
@@ -0,0 +1,38 @@
+class CreatePiroFirstCall < ActiveRecord::Migration[5.0]
+  def change
+    create_table :piro_first_calls do |t|
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+
+      t.string :hospital_name
+      t.string :hospital_city
+
+      t.string :place_of_birth
+      t.integer :date_of_birth
+      t.string :martial_status
+      t.integer :social_security
+      t.string :last_occupation
+      t.string :education_level
+      t.string :mothers_name
+      t.string :fathers_name
+      t.string :spouse_name
+
+      t.string :name_of_cemetery
+
+      t.string :kin_first_name
+      t.string :kin_last_name
+      t.string :kin_relationship
+      t.string :kin_street_address
+      t.string :kin_city
+      t.string :kin_state
+      t.integer :kin_zipcode
+      t.integer :kin_phone_number
+    end
+  end
+end
