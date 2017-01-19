674e473aeb3405eb6ea9dc9027cdfbe9f53324c4
pacosta613 <pedro.acosta613@gmail.com>
2017-01-19 12:58:48 -0500

    Completed writing out the actions of all controllers.

--- app/controllers/designation_of_intentions_controller.rb
+++ app/controllers/designation_of_intentions_controller.rb
@@ -1,19 +1,34 @@
 class DesignationOfIntentionsController < ApplicationController
 
+  def new
+    @intention = DesignationOfIntention.new
+  end
+
   def create
-    #.user_id = current_user.id
+    @intention = DesignationOfIntention.create(designation_of_intention_params)
+    @intention = DesignationOfIntention.user_id = current_user.id
+    if @intention.save
+
+      redirect_to @intention
+    end
   end
 
   def update
+    @intention = DesignationOfIntention.update(designation_of_intention_params)
+    if @intention.save
+
+      redirect_to @intention
+    end
   end
 
   def show
+    @intention = DesignationOfIntention.find(params[:id])
   end
 
   private
 
   def designation_of_intention_params
-    params.require(:designation_of_intention).permit(:name_of_deceased, :scheduled_cremation, :location_cremation, :phone_number, :address, :signature, :location_of_cemetery, :manner_of_disposition, :location, :date, :name)
+    params.require(:designation_of_intention).permit(:user_id, :name_of_deceased, :scheduled_cremation, :location_cremation, :phone_number, :address, :signature, :location_of_cemetery, :manner_of_disposition, :location, :date, :name)
   end
 
 end
--- app/controllers/written_statements_controller.rb
+++ app/controllers/written_statements_controller.rb
@@ -1,19 +1,33 @@
 class WrittenStatementsController < ApplicationController
 
+  def new
+    @statement = WrittenStatement.new
+  end
+
   def create
-    #.user_id = current_user.id
+    @statement = WrittenStatement.create(written_statements_params)
+    @statement = WrittenStatement.user_id = current_user.id
+    if @statement.save
+
+      redirect_to @statement
+    end
   end
 
   def update
+    @statement = WrittenStatement.update(written_statements_params)
+    if @statement
+
+      redirect_to @statement
+    end
   end
 
   def show
-    
+    @statement = WrittenStatement.find(params[:id])
   end
 
   private
 
   def written_statements_params
-    params.require(:written_statements).permit(:next_of_kin, :name_of_decease, :date)
+    params.require(:written_statements).permit(:user_id, :next_of_kin, :name_of_decease, :date)
   end
 end
