a60bdcf5f2a367928b5da309657b3fe9d6adf953
pacosta613 <pedro.acosta613@gmail.com>
2017-01-19 12:54:11 -0500

    Created the cremation permits actions create, new, show, and update. Working on Navbar.

--- app/controllers/cremation_permits_controller.rb
+++ app/controllers/cremation_permits_controller.rb
@@ -1,19 +1,34 @@
 class CremationPermitsController < ApplicationController
 
+  def new
+    @cremation = CremationPermit.new
+  end
+  
   def create
-    #.user_id = current_user.id
+    @cremation = CremationPermit.create(cremation_permits_params)
+    @cremation.user_id = current_user.id
+    if @cremation.save
+
+      redirect_to @cremation
+    end
   end
 
   def update
+    @cremation = CremationPermit.update(cremation_permits_params)
+    if @cremation.save
+
+      redirect_to @cremation
+    end
   end
 
   def show
+    @cremation = CremationPermit.find(params[:id])
   end
 
   private
 
   def cremation_permits_params
-    params.require(:cremation_permits).permit(:next_of_kin, :address, :relationship, :name_of_decease, :location_of_death, :date_of_death)
+    params.require(:cremation_permits).permit(:user_id, :next_of_kin, :address, :relationship, :name_of_decease, :location_of_death, :date_of_death)
   end
 
 end
